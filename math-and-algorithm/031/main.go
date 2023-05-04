ffc048f77e56533ea0ca6ffa0b3b597641f5c63d
nesheep <nesheep.dream@gmail.com>
2023-05-04 13:17:00 +0900

    :up:

--- math-and-algorithm/031/main.go
+++ math-and-algorithm/031/main.go
@@ -19,22 +19,18 @@ func main() {
 		fmt.Fscan(r, &a[i])
 	}
 
-	b := true
 	dp := make([]int, n+1)
 	dp[1] = a[1]
 	for i := 2; i < n+1; i++ {
-		if !b {
-			b = true
-			dp[i] = dp[i-1] + a[i]
-		} else {
-			if dp[i-2]+a[i] < dp[i-1] {
-				b = false
-				dp[i] = dp[i-1]
-			} else {
-				dp[i] = dp[i-2] + a[i]
-			}
-		}
+		dp[i] = max(dp[i-1], dp[i-2]+a[i])
 	}
 
 	fmt.Fprintln(w, dp[n])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
