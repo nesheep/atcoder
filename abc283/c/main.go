3cfcf18ac3fa7a51080109a32c9e1088702326da
nesheep <nesheep.dream@gmail.com>
2023-06-06 05:55:02 +0900

    abc283/c

---
+++ abc283/c/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []byte
+	fmt.Scan(&s)
+
+	var ans int
+	for i := 0; i < len(s); i++ {
+		ans++
+		if i == len(s)-1 {
+			break
+		}
+		if s[i] == '0' && s[i+1] == '0' {
+			i++
+		}
+	}
+
+	fmt.Println(ans)
+}
