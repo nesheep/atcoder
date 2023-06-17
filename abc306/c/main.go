d66c20a5a36bf7edd81e1d6ce509825adedf7225
nesheep <nesheep.dream@gmail.com>
2023-06-18 06:07:16 +0900

    abc306/c

--- abc306/c/main.go
+++ abc306/c/main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -16,30 +16,15 @@ func main() {
 	var n int
 	fmt.Fscan(rd, &n)
 
-	a := make([]int, n*3)
-	for i := 0; i < n*3; i++ {
-		fmt.Fscan(rd, &a[i])
-	}
-
-	e := make([]bool, n)
-	f := make([]int, n)
-	for i := range f {
-		f[i] = -1
-	}
-
-	for i := range a {
-		if !e[a[i]-1] {
-			e[a[i]-1] = true
-		} else if f[a[i]-1] == -1 {
-			f[a[i]-1] = i
-		}
-	}
-
-	sort.Ints(f)
-
+	var a int
+	cnt := make([]int, n)
 	ans := make([]string, 0, n)
-	for i := range f {
-		ans = append(ans, fmt.Sprint(a[f[i]]))
+	for i := 0; i < n*3; i++ {
+		fmt.Fscan(rd, &a)
+		cnt[a-1]++
+		if cnt[a-1] == 2 {
+			ans = append(ans, strconv.Itoa(a))
+		}
 	}
 
 	fmt.Fprintln(wr, strings.Join(ans, " "))
