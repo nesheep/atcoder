21ee856dca1ac9f85274db44b566b4ac9994928b
nesheep <nesheep.dream@gmail.com>
2023-11-05 05:17:38 +0900

    abc327/d

--- abc327/d/main.go
+++ abc327/d/main.go
@@ -6,15 +6,6 @@ import (
 	"os"
 )
 
-func contains(s []int, v int) bool {
-	for _, vv := range s {
-		if vv == v {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
 	rd := bufio.NewReader(os.Stdin)
 
@@ -31,16 +22,22 @@ func main() {
 		fmt.Fscan(rd, &b[i])
 	}
 
-	x := make([][]int, n+1)
+	x := make([]map[int]bool, n+1)
 	for i := range a {
-		x[a[i]] = append(x[a[i]], b[i])
-		x[b[i]] = append(x[b[i]], a[i])
+		if x[a[i]] == nil {
+			x[a[i]] = make(map[int]bool)
+		}
+		if x[b[i]] == nil {
+			x[b[i]] = make(map[int]bool)
+		}
+		x[a[i]][b[i]] = true
+		x[b[i]][a[i]] = true
 	}
 
 	for i := range x {
 		for j := range x[i] {
 			for k := range x[i] {
-				if contains(x[x[i][j]], x[i][k]) {
+				if x[j][k] {
 					fmt.Println("No")
 					os.Exit(0)
 				}
