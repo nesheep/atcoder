cfca0fd30ba716cd74d243af57318daeb5c7f966
nesheep <nesheep.dream@gmail.com>
2024-06-09 21:09:21 +0900

    abc356/d

--- abc356/d/main.go
+++ abc356/d/main.go
@@ -2,26 +2,23 @@ package main
 
 import "fmt"
 
-const d = 998244353
-
-func popcount(n uint64) uint64 {
-	var ans uint64
-	for n > 0 {
-		ans += n & 1
-		n >>= 1
-	}
-	return ans
-}
+const mod = 998244353
 
 func main() {
 	var n, m uint64
 	fmt.Scan(&n, &m)
 
 	var ans uint64
-	for i := uint64(0); i < n; i++ {
-		ans += popcount(i & m)
-		ans %= d
+	for i := uint64(0); i < 60; i++ {
+		if 1<<i&m != 0 {
+			c := (n + 1) / (2 << i)
+			r := (n + 1) % (2 << i)
+			ans += (c << i) % mod
+			if r > 1<<i {
+				ans += (r - (1 << i)) % mod
+			}
+		}
 	}
 
-	fmt.Println(ans)
+	fmt.Println(ans % mod)
 }
