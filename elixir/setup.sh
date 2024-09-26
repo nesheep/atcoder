981ac0e36752becc7bbaf5485519bd650d779e30
nesheep <nesheep.dream@gmail.com>
2024-09-26 21:53:41 +0900

    acs

--- elixir/setup.sh
+++ elixir/setup.sh
@@ -1,2 +1,3 @@
 alias mxn='mix new --module Main .'
 alias ojt='mix compile && oj t -c "mix run -e Main.main" -d ./tests -N'
+alias acs='acc s -- lib/main.ex --language 5085'
--- go/setup.sh
+++ go/setup.sh
@@ -1,1 +1,2 @@
 alias ojt='oj t -c "go run main.go" -d ./tests -N'
+alias acs='acc s main.go'
