cf7c90021174c7355d41e4890daf2ec10daebb07
nesheep <nesheep.dream@gmail.com>
2025-01-03 23:36:03 +0900

    rust/tessoku-book/src/bin/a03.rs

--- rust/tessoku-book/src/bin/a03.rs
+++ rust/tessoku-book/src/bin/a03.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: i64,
+        p: [i64; n],
+        q: [i64; n],
+    }
+
+    let ans = p.iter().any(|&x| q.iter().any(|&y| x + y == k));
+    println!("{}", if ans { "Yes" } else { "No" });
 }
