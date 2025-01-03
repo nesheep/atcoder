17abe4e315456bbca566661ca9ca0e6b4039928a
nesheep <nesheep.dream@gmail.com>
2025-01-04 00:34:38 +0900

    rust/tessoku-book/src/bin/a05.rs

--- rust/tessoku-book/src/bin/a05.rs
+++ rust/tessoku-book/src/bin/a05.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! { n: i64, k: i64 }
+
+    let mut ans = 0;
+    for x in 1..=n {
+        for y in 1..=n {
+            let z = k - x - y;
+            if z >= 1 && z <= n {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{ans}");
 }
