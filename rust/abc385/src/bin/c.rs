0e99cc78dbf3f4bed3e480426509eeaacd4331c8
nesheep <nesheep.dream@gmail.com>
2025-01-04 03:18:54 +0900

    rust/abc385/src/bin/c.rs

--- rust/abc385/src/bin/c.rs
+++ rust/abc385/src/bin/c.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: [i64; n],
+    }
+
+    let mut ans = 1;
+    for i in 1..n {
+        for j in 0..i {
+            let mut cnt = 0;
+            let mut g = -1;
+            let mut k = j;
+            while k < n {
+                if h[k] == g {
+                    cnt += 1;
+                    ans = ans.max(cnt);
+                } else {
+                    cnt = 1;
+                    g = h[k];
+                }
+                k += i;
+            }
+        }
+    }
+
+    println!("{ans}");
 }
