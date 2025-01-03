75a7acf4b9adca6b12156803198dca7d0b002ad5
nesheep <nesheep.dream@gmail.com>
2025-01-04 00:27:56 +0900

    rust/tessoku-book/src/bin/b04.rs

--- rust/tessoku-book/src/bin/b04.rs
+++ rust/tessoku-book/src/bin/b04.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! { n: Chars }
+
+    let ans = n
+        .iter()
+        .map(|&x| x.to_digit(10).unwrap())
+        .rev()
+        .enumerate()
+        .map(|(i, x)| x as i64 * 1 << i)
+        .sum::<i64>();
+
+    println!("{ans}");
 }
