0f9bd2c8e329e9d4c129d8ff29c98200770e613f
nesheep <nesheep.dream@gmail.com>
2025-01-03 23:19:47 +0900

    rust/tessoku-book/src/bin/a02.rs

--- rust/tessoku-book/src/bin/a02.rs
+++ rust/tessoku-book/src/bin/a02.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: i64,
+        a: [i64; n],
+    }
+
+    let ans = if a.contains(&x) { "Yes" } else { "No" };
+    println!("{ans}");
 }
