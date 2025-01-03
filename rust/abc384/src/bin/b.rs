470f299b5850fe1a008e69ccc0d7b65f54bfff34
nesheep <nesheep.dream@gmail.com>
2025-01-03 21:35:04 +0900

    abc384/b

--- rust/abc384/src/bin/b.rs
+++ rust/abc384/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        r: i64,
+        results: [(i8, i64); n],
+    }
+
+    let ans = results.iter().fold(r, |t, &(d, a)| match d {
+        1 if t >= 1600 && t < 2800 => t + a,
+        2 if t >= 1200 && t < 2400 => t + a,
+        _ => t,
+    });
+
+    println!("{ans}");
 }
