9eea82ce73f824c6efbba1361e5a8768f9c7dfbc
nesheep <nesheep.dream@gmail.com>
2025-01-06 04:27:42 +0900

    rust/abc382/src/bin/b.rs

--- rust/abc382/src/bin/b.rs
+++ rust/abc382/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        d: usize,
+        s: Chars,
+    }
+
+    let ans = s
+        .iter()
+        .rev()
+        .collect::<String>()
+        .replacen("@", ".", d)
+        .chars()
+        .rev()
+        .collect::<String>();
+
+    println!("{ans}")
 }
