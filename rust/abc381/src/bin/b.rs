de4ab05d727b2ff7d51e2f0868892f48133e3284
nesheep <nesheep.dream@gmail.com>
2025-01-07 03:19:03 +0900

    rust/abc381/src/bin/b.rs

--- rust/abc381/src/bin/b.rs
+++ rust/abc381/src/bin/b.rs
@@ -1,3 +1,22 @@
+use std::collections::HashSet;
+
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let ans = s
+        .iter()
+        .zip(s.iter().skip(1))
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a)
+        .collect::<HashSet<_>>()
+        .len()
+        * 2
+        == s.len();
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }
