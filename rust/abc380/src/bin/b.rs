66339a3f1e321b95456a0539c624aa041ffd55d9
nesheep <nesheep.dream@gmail.com>
2025-01-08 02:17:45 +0900

    rust/abc380/src/bin/b.rs

--- rust/abc380/src/bin/b.rs
+++ rust/abc380/src/bin/b.rs
@@ -1,3 +1,21 @@
+use itertools::Itertools;
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! { s: Chars }
+
+    let ans = s[1..]
+        .iter()
+        .fold((0i64, vec![]), |(cnt, mut ans), &c| match c {
+            '-' => (cnt + 1, ans),
+            '|' => {
+                ans.push(cnt);
+                (0, ans)
+            }
+            _ => (cnt, ans),
+        })
+        .1;
+
+    println!("{}", ans.iter().join(" "));
 }
