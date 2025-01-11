24437f3ab55ebdbd9d93d7a5ea274440d09c8c44
nesheep <nesheep.dream@gmail.com>
2025-01-11 15:56:07 +0900

    rust/abc378/src/bin/b.rs

--- rust/abc378/src/bin/b.rs
+++ rust/abc378/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
+fn calc(d: i64, (q, r): (i64, i64)) -> i64 {
+    (d / q + if d % q > r { 1 } else { 0 }) * q + r
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        rules: [(i64, i64); n],
+        q: usize,
+        questions: [(usize, i64); q],
+    }
+
+    let ans = questions.iter().map(|q| calc(q.1, rules[q.0 - 1]));
+
+    ans.for_each(|a| println!("{a}"));
 }
