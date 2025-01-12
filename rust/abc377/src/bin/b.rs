f13c5a4558e898f94ed7608866b852744ea6c755
nesheep <nesheep.dream@gmail.com>
2025-01-12 19:59:58 +0900

    rust/abc377/src/bin/b.rs

--- rust/abc377/src/bin/b.rs
+++ rust/abc377/src/bin/b.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! { s: [Chars; 8] }
+
+    let mut c = [true; 8];
+    let mut r = [true; 8];
+    for x in 0..8 {
+        for y in 0..8 {
+            if s[x][y] == '#' {
+                c[x] = false;
+                r[y] = false;
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for x in 0..8 {
+        for y in 0..8 {
+            if c[x] && r[y] {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{ans}");
 }
