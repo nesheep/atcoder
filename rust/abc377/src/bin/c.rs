1b4e874f8b6f75acd6e60490e5f161d384a7b659
nesheep <nesheep.dream@gmail.com>
2025-01-14 01:50:57 +0900

    rust/abc377/src/bin/c.rs

--- rust/abc377/src/bin/c.rs
+++ rust/abc377/src/bin/c.rs
@@ -1,3 +1,32 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        m: usize,
+        pieces: [(i64, i64); m],
+    }
+
+    let ds = [
+        (2, 1),
+        (1, 2),
+        (-1, 2),
+        (-2, 1),
+        (-2, -1),
+        (-1, -2),
+        (1, -2),
+        (2, -1),
+    ];
+
+    let m = pieces
+        .iter()
+        .flat_map(|p| ds.iter().map(move |d| (p.0 + d.0, p.1 + d.1)))
+        .filter(|p| p.0 >= 1 && p.0 <= n && p.1 >= 1 && p.1 <= n)
+        .chain(pieces.iter().cloned())
+        .collect::<HashSet<_>>();
+
+    let ans = n * n - m.len() as i64;
+    println!("{ans}");
 }
