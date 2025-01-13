df810cedb5225ccd91b4a5da6da68a44b3ee53b6
nesheep <nesheep.dream@gmail.com>
2025-01-13 14:28:27 +0900

    rust/abc378/src/bin/c.rs

--- rust/abc378/src/bin/c.rs
+++ rust/abc378/src/bin/c.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let mut m = HashMap::new();
+    let mut b = vec![0; n];
+
+    for i in 0..n {
+        b[i] = match m.get(&a[i]) {
+            Some(v) => *v,
+            None => -1,
+        };
+
+        m.insert(a[i], i as i64 + 1);
+    }
+
+    let ans = b.iter().map(|v| v.to_string()).join(" ");
+    println!("{ans}");
 }
