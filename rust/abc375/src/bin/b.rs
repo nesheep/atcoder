c226348e31a46a1b15968fac92bcbceadc361f09
nesheep <nesheep.dream@gmail.com>
2025-01-18 00:25:17 +0900

    rust/abc375/src/bin/b.rs

--- rust/abc375/src/bin/b.rs
+++ rust/abc375/src/bin/b.rs
@@ -1,3 +1,25 @@
+use std::iter;
+
+use itertools::Itertools;
+use proconio::input;
+
+fn cost((x1, y1): &(f64, f64), (x2, y2): &(f64, f64)) -> f64 {
+    ((x1 - x2).powf(2.0) + (y1 - y2).powf(2.0)).sqrt()
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        ps: [(f64, f64); n],
+    }
+
+    let o = (0.0, 0.0);
+
+    let ans = iter::once(&o)
+        .chain(ps.iter())
+        .chain(iter::once(&o))
+        .tuple_windows::<(_, _)>()
+        .fold(0.0, |acc, (a, b)| acc + cost(a, b));
+
+    println!("{ans:.20}");
 }
