761ec4e668b13c19c711ff6d3bce9b7cc9c42c86
nesheep <nesheep.dream@gmail.com>
2025-01-18 16:54:29 +0900

    rust/abc374/src/bin/b.rs

--- rust/abc374/src/bin/b.rs
+++ rust/abc374/src/bin/b.rs
@@ -1,3 +1,24 @@
+use std::iter;
+
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let l = s.len().max(t.len());
+
+    let si = s.iter().chain(iter::repeat(&'.')).take(l);
+    let ti = t.iter().chain(iter::repeat(&'.')).take(l);
+
+    let ans = si
+        .zip(ti)
+        .position(|(&s, &t)| s != t)
+        .map(|i| i + 1)
+        .unwrap_or(0);
+
+    println!("{ans}");
 }
