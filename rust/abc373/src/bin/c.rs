b2c910deb870f9be7a799d84108b8b116d571ab6
nesheep <nesheep.dream@gmail.com>
2025-01-19 01:11:45 +0900

    rust/abc373/src/bin/c.rs

--- rust/abc373/src/bin/c.rs
+++ rust/abc373/src/bin/c.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+        b: [i64; n],
+    }
+
+    let ans = a.iter().max().unwrap() + b.iter().max().unwrap();
+
+    println!("{ans}");
 }
