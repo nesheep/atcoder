4ed2e12334ce6a3324ea2be1c02b05d1683ada63
nesheep <nesheep.dream@gmail.com>
2025-01-19 01:03:28 +0900

    rust/abc373/src/bin/b.rs

--- rust/abc373/src/bin/b.rs
+++ rust/abc373/src/bin/b.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! { s: Chars }
+
+    let ans = s
+        .iter()
+        .enumerate()
+        .sorted_by_key(|x| x.1)
+        .map(|x| x.0 as i64)
+        .tuple_windows()
+        .fold(0, |acc, (a, b)| acc + (b - a).abs());
+
+    println!("{ans}");
 }
