6c9264322247a4833543f8f4b4dfcd470cc3469e
nesheep <nesheep.dream@gmail.com>
2025-01-21 03:46:28 +0900

    rust/abc389/src/bin/b.rs

--- rust/abc389/src/bin/b.rs
+++ rust/abc389/src/bin/b.rs
@@ -1,3 +1,20 @@
+use std::ops::ControlFlow;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! { x: usize }
+
+    let ans = (1..=50)
+        .try_fold(1, |acc, i| {
+            if acc >= x {
+                ControlFlow::Break(i - 1)
+            } else {
+                ControlFlow::Continue(acc * i)
+            }
+        })
+        .break_value()
+        .unwrap();
+
+    println!("{ans}");
 }
