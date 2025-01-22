6d209ac72487e20ec261da1c1d559e8a274d13bb
nesheep <nesheep.dream@gmail.com>
2025-01-23 04:33:01 +0900

    rust/abc372/src/bin/b.rs

--- rust/abc372/src/bin/b.rs
+++ rust/abc372/src/bin/b.rs
@@ -1,3 +1,27 @@
+use std::iter;
+
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! { mut m: i64 }
+
+    let ans = iter::from_fn(move || {
+        (0..11)
+            .map(|x| 3i64.pow(x))
+            .enumerate()
+            .rev()
+            .find_map(|(i, x)| {
+                if x <= m {
+                    m -= x;
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+    })
+    .collect::<Vec<_>>();
+
+    println!("{}", ans.len());
+    println!("{}", ans.iter().map(ToString::to_string).join(" "));
 }
