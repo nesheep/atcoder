b4aaa0655d7ba195c45e3aae818c70a828f4240c
nesheep <nesheep.dream@gmail.com>
2025-01-25 12:35:00 +0900

    rust/abc371/src/bin/b.rs

--- rust/abc371/src/bin/b.rs
+++ rust/abc371/src/bin/b.rs
@@ -1,3 +1,24 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        children: [(usize, char); m],
+    }
+
+    let ans = children
+        .iter()
+        .scan(HashSet::with_capacity(n), |s, &(a, b)| {
+            if b == 'M' && !s.contains(&a) {
+                s.insert(a);
+                Some(true)
+            } else {
+                Some(false)
+            }
+        });
+
+    ans.for_each(|a| println!("{}", if a { "Yes" } else { "No" }));
 }
