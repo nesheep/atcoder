5c65e8de12a3368cb8267f4ea6e19a0e33d3bd11
nesheep <nesheep.dream@gmail.com>
2025-01-28 04:38:49 +0900

    rust/abc370/src/bin/b.rs

--- rust/abc370/src/bin/b.rs
+++ rust/abc370/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
+fn get(a: &[usize], i: usize, j: usize) -> usize {
+    a[i * (i + 1) / 2 + j] - 1
+}
+
+fn combine(a: &[usize], i: usize, j: usize) -> usize {
+    if i >= j {
+        get(a, i, j)
+    } else {
+        get(a, j, i)
+    }
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n * (n + 1) / 2],
+    }
+
+    let ans = (0..n).fold(0, |i, j| combine(&a, i, j)) + 1;
+
+    println!("{ans}");
 }
