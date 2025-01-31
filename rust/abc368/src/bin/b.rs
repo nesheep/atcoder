b51664c7bd813c285f548e86f1fae799a138db22
nesheep <nesheep.dream@gmail.com>
2025-02-01 07:11:01 +0900

    rust/abc368/src/bin/b.rs

--- rust/abc368/src/bin/b.rs
+++ rust/abc368/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    }
+
+    let mut ans = 0;
+    loop {
+        let cnt = a.iter().filter(|&&v| v > 0).count();
+        match cnt {
+            0 | 1 => break,
+            _ => {
+                a.sort_by(|a, b| b.cmp(a));
+                a[0] -= 1;
+                a[1] -= 1;
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{ans}");
 }
