b218c2695c2bc72724a9d94261a7f69c72b0f187
nesheep <nesheep.dream@gmail.com>
2025-02-04 03:55:08 +0900

    rust/tessoku-book/src/bin/b08.rs

--- rust/tessoku-book/src/bin/b08.rs
+++ rust/tessoku-book/src/bin/b08.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        points: [(usize, usize); n],
+        q: usize,
+        questions: [(usize, usize, usize, usize); q],
+    }
+
+    let mut t = vec![vec![0; 1501]; 1501];
+    for &(x, y) in points.iter() {
+        t[x][y] += 1;
+    }
+    for i in 0..1500 {
+        for j in 0..1500 {
+            t[i + 1][j + 1] += t[i + 1][j];
+        }
+    }
+    for i in 0..1500 {
+        for j in 0..1500 {
+            t[i + 1][j + 1] += t[i][j + 1];
+        }
+    }
+
+    let ans = questions
+        .iter()
+        .map(|&(y1, x1, y2, x2)| t[y2][x2] - t[y1 - 1][x2] - t[y2][x1 - 1] + t[y1 - 1][x1 - 1]);
+
+    ans.for_each(|a| println!("{a}"));
 }
