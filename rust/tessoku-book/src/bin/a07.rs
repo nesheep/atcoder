3703e8785745192a41aea145d2046d6ba866745c
nesheep <nesheep.dream@gmail.com>
2025-02-03 09:13:21 +0900

    rust/tessoku-book/src/bin/a07.rs

--- rust/tessoku-book/src/bin/a07.rs
+++ rust/tessoku-book/src/bin/a07.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        d: usize,
+        n: usize,
+        ranges: [(usize, usize); n],
+    }
+
+    let mut a = vec![0; d + 2];
+    for &(l, r) in ranges.iter() {
+        a[l] += 1;
+        a[r + 1] -= 1;
+    }
+    for i in 1..=d {
+        a[i] += a[i - 1];
+    }
+
+    a[1..=d].iter().for_each(|a| println!("{a}"));
 }
