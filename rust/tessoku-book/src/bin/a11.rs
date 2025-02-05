67c595249472667274e9c416bda17eb4d21ad693
nesheep <nesheep.dream@gmail.com>
2025-02-06 08:24:52 +0900

    rust/tessoku-book/src/bin/a11.rs

--- rust/tessoku-book/src/bin/a11.rs
+++ rust/tessoku-book/src/bin/a11.rs
@@ -1,3 +1,26 @@
+use std::cmp::Ordering::*;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize; n],
+    }
+
+    let ans = {
+        let mut l = 0;
+        let mut r = n - 1;
+        loop {
+            let m = (l + r) / 2;
+            match Ord::cmp(&x, &a[m]) {
+                Less => r = m - 1,
+                Equal => break m,
+                Greater => l = m + 1,
+            }
+        }
+    };
+
+    println!("{}", ans + 1);
 }
