5fe8c1e370baa5ad402a81ff66412c32c5597360
nesheep <nesheep.dream@gmail.com>
2025-02-06 08:59:22 +0900

    rust/tessoku-book/src/bin/a12.rs

--- rust/tessoku-book/src/bin/a12.rs
+++ rust/tessoku-book/src/bin/a12.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
+fn f(a: &[usize], t: usize) -> usize {
+    a.iter().map(|&x| t / x).sum::<usize>()
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let ans = {
+        let mut l = 0;
+        let mut r = 1_000_000_000;
+        loop {
+            if l == r {
+                break l;
+            }
+            let m = (l + r) / 2;
+            if f(&a, m) < k {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+    };
+
+    println!("{ans}");
 }
