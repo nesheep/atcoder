347ab8e279f437341452eaaf2289e64075ba0300
nesheep <nesheep.dream@gmail.com>
2025-02-07 19:37:40 +0900

    rust/tessoku-book/src/bin/a13.rs

--- rust/tessoku-book/src/bin/a13.rs
+++ rust/tessoku-book/src/bin/a13.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let ans = a
+        .iter()
+        .scan(0, |r, &x| {
+            while *r < n - 1 && a[*r + 1] - x <= k {
+                *r += 1;
+            }
+            Some(*r)
+        })
+        .enumerate()
+        .map(|(l, r)| r - l)
+        .sum::<usize>();
+
+    println!("{ans}");
 }
