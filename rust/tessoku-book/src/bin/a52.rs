f0d292a1c63b926fa11b7c1474914acfac5809bb
nesheep <nesheep.dream@gmail.com>
2025-02-08 03:55:30 +0900

    rust/tessoku-book/src/bin/a52.rs

--- rust/tessoku-book/src/bin/a52.rs
+++ rust/tessoku-book/src/bin/a52.rs
@@ -1,3 +1,41 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
+enum Query {
+    Query1(String),
+    Query2,
+    Query3,
+}
+
 fn main() {
-    todo!();
+    input! { q: usize }
+
+    let queries = (0..q).map(|_| {
+        input! { query: usize }
+        match query {
+            1 => {
+                input! { s: String }
+                Query::Query1(s)
+            }
+            2 => Query::Query2,
+            3 => Query::Query3,
+            _ => unreachable!(),
+        }
+    });
+
+    let mut queue = VecDeque::new();
+    for query in queries {
+        match query {
+            Query::Query1(s) => queue.push_back(s),
+            Query::Query2 => {
+                if let Some(s) = queue.front() {
+                    println!("{s}");
+                }
+            }
+            Query::Query3 => {
+                queue.pop_front();
+            }
+        }
+    }
 }
