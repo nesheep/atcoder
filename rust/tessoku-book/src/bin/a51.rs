4e1cbe5f225a89d88925c6e1c15bed3c10ad3d03
nesheep <nesheep.dream@gmail.com>
2025-02-08 03:46:59 +0900

    rust/tessoku-book/src/bin/a51.rs

--- rust/tessoku-book/src/bin/a51.rs
+++ rust/tessoku-book/src/bin/a51.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
+enum Query {
+    Query1(String),
+    Query2,
+    Query3,
+}
+
 fn main() {
-    todo!();
+    input! { q: usize }
+
+    let queries = (0..q).map(|_| {
+        input! { query: usize }
+        match query {
+            1 => {
+                input! { s: String }
+                Query::Query1(s)
+            }
+            2 => Query::Query2,
+            3 => Query::Query3,
+            _ => unreachable!(),
+        }
+    });
+
+    let mut stack = vec![];
+    for query in queries {
+        match query {
+            Query::Query1(s) => stack.push(s),
+            Query::Query2 => {
+                println!("{}", stack[stack.len() - 1]);
+            }
+            Query::Query3 => {
+                stack.pop();
+            }
+        }
+    }
 }
