4ad3ab348cc185c4253ee1acc3989daf7bf39983
nesheep <nesheep.dream@gmail.com>
2025-02-08 10:46:17 +0900

    rust/tessoku-book/src/bin/a61.rs

--- rust/tessoku-book/src/bin/a61.rs
+++ rust/tessoku-book/src/bin/a61.rs
@@ -1,3 +1,21 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        edges: [(usize, usize); m],
+    }
+
+    let mut graph = vec![vec![]; n + 1];
+    for &(a, b) in edges.iter() {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    for i in 1..=n {
+        let s = graph[i].iter().sorted().map(ToString::to_string).join(", ");
+        println!("{i}: {{{s}}}");
+    }
 }
