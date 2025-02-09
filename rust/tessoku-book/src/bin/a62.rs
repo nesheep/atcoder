63aecf70952a79b29a09183de0f44115490caed2
nesheep <nesheep.dream@gmail.com>
2025-02-10 04:41:20 +0900

    rust/tessoku-book/src/bin/a62.rs

--- rust/tessoku-book/src/bin/a62.rs
+++ rust/tessoku-book/src/bin/a62.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
+fn dfs(graph: &Vec<Vec<usize>>, v: usize, visited: &mut Vec<bool>) {
+    visited[v] = true;
+    for &u in graph[v].iter() {
+        if visited[u] {
+            continue;
+        }
+        dfs(graph, u, visited);
+    }
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        edges: [(usize, usize); m],
+    }
+
+    let mut graph = vec![vec![]; n + 1];
+    for &(a, b) in edges.iter() {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut visited = vec![false; n + 1];
+    dfs(&graph, 1, &mut visited);
+    let ans = visited[1..].iter().all(|&x| x);
+
+    println!(
+        "{}",
+        if ans {
+            "The graph is connected."
+        } else {
+            "The graph is not connected."
+        }
+    );
 }
