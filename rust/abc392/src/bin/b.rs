e7fca16560998cc2b48a3dc121dfaaf553646758
nesheep <nesheep.dream@gmail.com>
2025-02-15 04:59:51 +0900

    rust/abc392/src/bin/b.rs

--- rust/abc392/src/bin/b.rs
+++ rust/abc392/src/bin/b.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; m],
+    }
+
+    let ans = (1..=n)
+        .filter(|&x| a.iter().all(|&y| x != y))
+        .collect::<Vec<_>>();
+
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
