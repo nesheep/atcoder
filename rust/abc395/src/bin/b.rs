4463167907bc04915f6e12ccdde8bdd5f21efd1e
nesheep <nesheep.dream@gmail.com>
2025-03-05 14:00:47 +0900

    src/bin/b.rs

--- rust/abc395/src/bin/b.rs
+++ rust/abc395/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! { n: usize }
+
+    let ans = (1..=n).map(|x| {
+        (1..=n).map(move |y| {
+            if x.min(n + 1 - x).min(y).min(n + 1 - y) % 2 == 0 {
+                '.'
+            } else {
+                '#'
+            }
+        })
+    });
+
+    ans.for_each(|l| println!("{}", l.collect::<String>()));
 }
