3918a5efd03f009ba10cd4a3f8e619929111c6a4
nesheep <nesheep.dream@gmail.com>
2025-03-15 11:48:47 +0900

    rust/abc362/src/bin/b.rs

--- rust/abc362/src/bin/b.rs
+++ rust/abc362/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
+fn distance(p1: (i64, i64), p2: (i64, i64)) -> i64 {
+    (p1.0 - p2.0).pow(2) + (p1.1 - p2.1).pow(2)
+}
+
 fn main() {
-    todo!();
+    input! {
+        p1: (i64, i64),
+        p2: (i64, i64),
+        p3: (i64, i64),
+    }
+
+    let mut d = [distance(p1, p2), distance(p1, p3), distance(p2, p3)];
+    d.sort();
+    let ans = d[1] + d[0] == d[2];
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }
