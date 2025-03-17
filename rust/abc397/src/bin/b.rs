fbbe0d15451f4e42af3ece8d1caa3fa7e45050d0
nesheep <nesheep.dream@gmail.com>
2025-03-18 04:20:58 +0900

    rust/abc397/src/bin/b.rs

--- rust/abc397/src/bin/b.rs
+++ rust/abc397/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! { s: Chars }
+
+    let ans = match s.iter().fold((0, 0), |(i, a), &c| match (i, c) {
+        (i, 'i') if i % 2 == 0 => (i + 1, a),
+        (i, 'o') if i % 2 == 1 => (i + 1, a),
+        _ => (i + 2, a + 1),
+    }) {
+        (i, a) if i % 2 == 1 => a + 1,
+        (_, a) => a,
+    };
+
+    println!("{ans}");
 }
