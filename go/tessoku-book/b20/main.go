32d502b2210bea755f4ce0eddf1dc62c6e76d583
nesheep <nesheep.dream@gmail.com>
2025-08-15 19:59:10 +0900

    go/tessoku-book/b20/main.go

---
+++ go/tessoku-book/b20/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s, t []byte
+	fmt.Scan(&s, &t)
+
+	dp := make([][]int, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(t)+1)
+	}
+
+	for i := range dp {
+		dp[i][0] = i
+	}
+	for j := range dp[0] {
+		dp[0][j] = j
+	}
+
+	for i := range dp {
+		for j := range dp[i] {
+			if i == 0 || j == 0 {
+				continue
+			}
+			dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1)
+			if s[i-1] == t[j-1] {
+				dp[i][j] = min(dp[i][j], dp[i-1][j-1])
+			} else {
+				dp[i][j] = min(dp[i][j], dp[i-1][j-1]+1)
+			}
+		}
+	}
+
+	fmt.Println(dp[len(s)][len(t)])
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
